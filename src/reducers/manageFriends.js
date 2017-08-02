071207cc01842cc37e28db721ba0fe1c525ebab0
Jake A Musich <makejusich@gmail.com>
2017-08-01 22:25:35 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,26 @@
 export function manageFriends(state, action){
+  console.log(state)
+  switch(action.type) {
+    case 'ADD_FRIEND': {
+      return {friends: [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id} ]}
+    }
+    case 'REMOVE_FRIEND': {
+      let indexToDelete = null
+        state.friends.map( (friend, index ) => {
+        if (friend .id === action.id) {
+          indexToDelete = index
+        }
+      })
+
+      return {
+        friends:
+        [
+          ...state.friends.slice(0, indexToDelete),
+          ...state.friends.slice(indexToDelete + 1)
+        ]
+      }
+     }
+    default:
+      return state
+  }
 }
